ffc2dd8a2ce639e8d018889278a1db41a45d6a08
himoura32 <shawn.ahearn@outlook.com>
2022-06-15 09:51:37 -0400

    fixed path.dirname issue

--- models/product.js
+++ models/product.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(process.mainModule.filename),
+  path.dirname(require.main.filename),
   'data',
   'products.json'
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -23,9 +23,9 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
