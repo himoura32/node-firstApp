146f028d7e489552cdc6360e1959fbcc3c06e462
himoura32 <shawn.ahearn@outlook.com>
2022-06-17 18:51:55 -0400

    coded Sequelize, coded model/synced db/fetchAll

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'Tschuss13!', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'Tschuss13!',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
