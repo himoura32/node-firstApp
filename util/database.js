2da6f8e8c2ef7438244655334db91e3aff667523
himoura32 <shawn.ahearn@outlook.com>
2022-06-19 11:57:51 -0400

    coded mongoDb connections/created shop collection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,38 @@
-const { Sequelize } = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'Tschuss13!', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://himoura:Bargle33@cluster0.z9d0q.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then((client) => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('node-complete', 'root', 'Tschuss13!', {
+//   dialect: 'mysql',
+//   host: 'localhost',
+// });
+
+// module.exports = sequelize;
