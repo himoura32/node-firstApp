1c26c552025b9ec99d3f0cdfc34032467c2afbde
himoura32 <shawn.ahearn@outlook.com>
2022-06-20 09:47:37 -0400

    coded Mongoose db connection/save data

--- app.js
+++ app.js
@@ -2,17 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const mongoose = require('mongoose');
 
-// const sequelize = require('./util/database');
-// const Product = require('./models/product');
+const errorController = require('./controllers/error');
 // const User = require('./models/user');
-// const Cart = require('./models/cart');
-// const CartItem = require('./models/cart-item');
-// const Order = require('./models/order');
-// const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -25,53 +18,27 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('62af8af35d58d9744b616e95')
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('62af8af35d58d9744b616e95')
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
-
-// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-// User.hasMany(Product);
-// User.hasOne(Cart);
-// Cart.belongsTo(User);
-// Cart.belongsToMany(Product, { through: CartItem });
-// Product.belongsToMany(Cart, { through: CartItem });
-// Order.belongsTo(User);
-// User.hasMany(Order);
-// Order.belongsToMany(Product, { through: OrderItem });
-
-// sequelize
-//   // .sync({ force: true })
-//   .sync()
-//   .then((result) => {
-//     return User.findByPk(1);
-//   })
-//   .then((user) => {
-//     if (!user) {
-//       return User.create({ name: 'Max', email: 'test@test.com' });
-//     }
-//     return user;
-//   })
-//   .then((user) => {
-//     // console.log(user);
-//     return user.createCart();
-//   })
-//   .then(cart => {
-//     app.listen(3000);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+mongoose
+  .connect(
+    'mongodb+srv://himoura:Bargle33@cluster0.z9d0q.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
