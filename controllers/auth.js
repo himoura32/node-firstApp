08d5036c85bb96fcea7f68a49f93badae6d7d8e2
himoura32 <shawn.ahearn@outlook.com>
2022-06-22 08:24:47 -0400

    coded authController signup/routes/ejs

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,6 +9,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById('62b0a78d80905b44034494d4')
     .then((user) => {
@@ -21,6 +29,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
